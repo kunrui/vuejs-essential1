1be48a24fc6715619ec2271f4ecaba5493ead3a9
rui <szws_rui@git.cloud.tencent.com>
2018-07-16 18:53:51 +0800

    修改用户信息

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 引入 ./routes.js 的默认值
+import routes from './routes'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  linkExactActiveClass: 'active',
+  routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  const app = router.app
+  const store = app.$options.store
+  const auth = router.app.$options.store.state.auth
+
+  app.$message.hide()
+
+  if (
+    (auth && to.path.indexOf('/auth/') !== -1) ||
+    (!auth && to.meta.auth)
+  ) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
